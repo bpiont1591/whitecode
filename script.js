470839d2567dceb671946c435524be8849a6d13c
bpiont1591 <129689696+bpiont1591@users.noreply.github.com>
2026-03-09 11:17:12 +0100

    x

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const API_CONTACT = "/contact";
 const API_ME = "/auth/me";
 const API_LOGOUT = "/auth/logout";
 const API_DISCORD_STATUS = "/discord/status";
-const API_OPINIONS = "https://api.whitecode.pl/api/opinions";
+const API_OPINIONS = "http://api.whitecode.pl:10005/api/opinions";
 
 const form = document.getElementById("contactForm");
 const statusEl = document.getElementById("status");
