62b9497893ecc8d686bc2515477b5d410afffbc6
bpiont1591 <129689696+bpiont1591@users.noreply.github.com>
2026-03-09 13:16:25 +0100

    Update API_OPINIONS endpoint URLx

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const API_CONTACT = "/contact";
 const API_ME = "/auth/me";
 const API_LOGOUT = "/auth/logout";
 const API_DISCORD_STATUS = "/discord/status";
-const API_OPINIONS = "https://api.whitecode.pl/api/opinions";;
+const API_OPINIONS = "http://193.111.250.101:10005/api/opinions";
+
 
 const form = document.getElementById("contactForm");
 const statusEl = document.getElementById("status");
